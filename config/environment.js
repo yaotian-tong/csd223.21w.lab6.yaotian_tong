14fd433ad41de16f284dcb2b83e7284931dd0081
YaoTian Tong <tongyaotian@sina.cn>
2021-04-25 13:20:32 -0400

    lab6

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN ="pk.eyJ1IjoidG9uZzEyMzQiLCJhIjoiY2tueGJkbG5uMGp4ZjJzbzhham0wcG45ciJ9.1X5LsFW0Hj_6CtOvcZGq5w";
+
   return ENV;
 };
